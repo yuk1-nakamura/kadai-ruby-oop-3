78f89e1b865e323461f8e1b78412664a5d439499
yuki.nakamura <n-yuki@kjb.biglobe.ne.jp>
2021-08-28 16:53:56 +0000

    add animal.rb

---
+++ human.rb
@@ -0,0 +1,15 @@
+require './animal'
+require './thinkable'
+
+class Human < Animal
+    include Thinkable
+    
+    attr_accessor :name, :age, :hobby
+    
+    def initialize(name,age,hobby)
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+    end
+end
+    
