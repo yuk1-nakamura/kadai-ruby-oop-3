a47050b67cb185f009a975ecd6663b96515eb09f
yuki.nakamura <n-yuki@kjb.biglobe.ne.jp>
2021-08-29 14:24:23 +0000

    add human.rb

--- human.rb
+++ human.rb
@@ -4,12 +4,10 @@ require './thinkable'
 class Human < Animal
     include Thinkable
     
-    attr_accessor :name, :age, :hobby
+    attr_accessor :hobby
     
     def initialize(name,age,hobby)
-        self.name = name
-        self.age = age
+        super(name,age)
         self.hobby = hobby
     end
 end
-    
